f689ea5dee08c0c084a177daac44aca1871493d9
Quyunshuo <quyunshuo@qq.com>
2020-05-14 18:11:19 +0800

    Modify: 轮播图 列表

--- pages/steamer/steamer.js
+++ pages/steamer/steamer.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 轮播图的url
+    swiperItem: ["https://gank.io/images/e92911f5ff9446d5a899b652b1934b93",
+      "https://gank.io/images/95ddb01b6bd34a85aedfda4c9e9bd003",
+      "https://gank.io/images/4817628a6762410895f814079a6690a1"
+    ],
+    // 八个女孩的数据
+    beautyList: [{
+      imgurl: "https://img2.woyaogexing.com/2020/05/13/e8a1e9f885e04816abd1c32360a63ee8!400x400.jpeg",
+      name: "1号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/13/9da6d8ca9c5642789fc248c3c56ed04e!400x400.jpeg",
+      name: "2号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/13/08c9d870fda64606a5439e02984a2780!400x400.jpeg",
+      name: "3号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/13/fc483cb8156b48148df7bf4875cb4c69!400x400.jpeg",
+      name: "4号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/12/2c9a4a5d96344e0cabb2f97ff8247679!400x400.jpeg",
+      name: "5号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/13/64cb380e736c4731902aea818e8838e7!400x400.jpeg",
+      name: "6号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/12/2aee922fc3f54bc7ac3fa6be367405f5!400x400.jpeg",
+      name: "7号"
+    }, {
+      imgurl: "https://img2.woyaogexing.com/2020/05/12/d12e825f35b74302bd57283c49e93ce9!400x400.jpeg",
+      name: "8号"
+    }],
   },
 
   /**
